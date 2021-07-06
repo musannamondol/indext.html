ebd5ae9c6d747edad3e5f5742fb49c9fd59e9f3b
Nazrul Islam <nazruldiu26@gmail.com>
2021-07-06 18:00:39 +0600

    first commit

---
+++ form-handler.php
@@ -0,0 +1,40 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject =$_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 'info@yourwebsite.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name.\n".
+				"User Email: $visitor_email.\n".
+				 "subject: $subject.\n".
+				 "User Message: $message.\n";
+
+$to = 'musannarajbari123@gmail.com';
+
+$headers ="From: $email_from \r\n";
+
+$headers .="Reply_To: $visitor_email \r\n";
+
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
